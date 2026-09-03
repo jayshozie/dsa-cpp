eb921491f980e99a6797d1ac8d833b655c5b5240
terra2o <terra2o@protonmail.com>
2026-09-03 21:41:44 +0300

    formatted code

--- tests/linear/LinkedList.cpp
+++ tests/linear/LinkedList.cpp
@@ -22,81 +22,81 @@ import dsa.linear.LinkedList;
 
 void test_constructors()
 {
-    dsa::LinkedList<int> empty_list;
-    assert(empty_list.empty());
-    assert(empty_list.size() == 0);
+	dsa::LinkedList<int> empty_list;
+	assert(empty_list.empty());
+	assert(empty_list.size() == 0);
 
-    dsa::LinkedList<int> list{1, 2, 3, 4, 5};
-    assert(!list.empty());
-    assert(list.size() == 5);
-    assert(list.front() == 1);
+	dsa::LinkedList<int> list{1, 2, 3, 4, 5};
+	assert(!list.empty());
+	assert(list.size() == 5);
+	assert(list.front() == 1);
 }
 
 void test_push_and_pop()
 {
-    dsa::LinkedList<std::string> list;
+	dsa::LinkedList<std::string> list;
 
-    list.pushFront("world");
-    list.pushFront("hello");
+	list.pushFront("world");
+	list.pushFront("hello");
 
-    assert(list.size() == 2);
-    assert(list.front() == "hello");
+	assert(list.size() == 2);
+	assert(list.front() == "hello");
 
-    auto popped = list.popFront();
-    assert(popped.has_value());
-    assert(*popped == "hello");
-    assert(list.size() == 1);
-    assert(list.front() == "world");
+	auto popped = list.popFront();
+	assert(popped.has_value());
+	assert(*popped == "hello");
+	assert(list.size() == 1);
+	assert(list.front() == "world");
 
-    list.popFront();
-    assert(list.empty());
+	list.popFront();
+	assert(list.empty());
 
-    auto empty_pop = list.popFront();
-    assert(!empty_pop.has_value());
+	auto empty_pop = list.popFront();
+	assert(!empty_pop.has_value());
 }
 
 void test_copy_and_move()
 {
-    dsa::LinkedList<int> original{10, 20, 30};
+	dsa::LinkedList<int> original{10, 20, 30};
 
-    dsa::LinkedList<int> copy(original);
-    assert(copy.size() == 3);
-    assert(copy.front() == 10);
+	dsa::LinkedList<int> copy(original);
+	assert(copy.size() == 3);
+	assert(copy.front() == 10);
 
-    dsa::LinkedList<int> moved(std::move(copy));
-    assert(moved.size() == 3);
-    assert(moved.front() == 10);
+	dsa::LinkedList<int> moved(std::move(copy));
+	assert(moved.size() == 3);
+	assert(moved.front() == 10);
 
-    dsa::LinkedList<int> assigned;
-    assigned = original;
-    assert(assigned.size() == 3);
+	dsa::LinkedList<int> assigned;
+	assigned = original;
+	assert(assigned.size() == 3);
 
-    dsa::LinkedList<int> move_assigned;
-    move_assigned = std::move(assigned);
-    assert(move_assigned.size() == 3);
+	dsa::LinkedList<int> move_assigned;
+	move_assigned = std::move(assigned);
+	assert(move_assigned.size() == 3);
 }
 
 void test_iteration()
 {
-    dsa::LinkedList<int> list{1, 2, 3};
+	dsa::LinkedList<int> list{1, 2, 3};
 
-    int expected = 1;
-    for (int val : list) {
-        assert(val == expected++);
-    }
+	int expected = 1;
+	for (int val : list) {
+		assert(val == expected++);
+	}
 
-    const auto &const_ref = list;
-    auto it = const_ref.cbegin();
-    assert(*it == 1);
-    assert(it == list.begin());
+	const auto &const_ref = list;
+	auto it = const_ref.cbegin();
+	assert(*it == 1);
+	assert(it == list.begin());
 }
 
 int main()
 {
-    test_constructors();
-    test_push_and_pop();
-    test_copy_and_move();
-    test_iteration();
+	test_constructors();
+	test_push_and_pop();
+	test_copy_and_move();
+	test_iteration();
 
-    return 0;
+	return 0;
 }
