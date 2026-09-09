85ccf80304b4655d2e37f0bef4355fb153b2cb96
Emir Baha Yıldırım <jayshozie@gmail.com>
2026-09-09 07:27:01 +0300

    chore(legal): Audit copyright headers and normalize author casing
    * Update author name capitalization to "Emir Baha Yıldırım" across root
    configuration files
    * Add full GPLv3 header blocks to `ArrayStack`, `Linear`, and
    `DoublyLinkedList` tests
    * Remove coauthor attribution from modules authored independently
    
    Signed-off-by: Emir Baha Yıldırım <jayshozie@gmail.com>

--- .nvim.lua
+++ .nvim.lua
@@ -1,6 +1,5 @@
 -- dsa-cpp - An implementation of some data structures and algorithms in C++.
--- Copyright (C)  2026  Emir Baha YILDIRIM <jayshozie@gmail.com>
--- Copyright (C)  2026  terra2o <terra2o@protonmail.com>
+-- Copyright (C)  2026  Emir Baha Yıldırım <jayshozie@gmail.com>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
